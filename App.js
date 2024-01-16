f5cbbabd1b1e3ab4e0becc7196d4ea4deb4bb762
crcascr <cristianj_20@hotmail.com>
2024-01-16 16:31:04 -0500

    Implementacion StatusBar

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
 import React from "react";
 import { NativeRouter } from "react-router-native";
+import { StatusBar } from "expo-status-bar";
 
 import Main from "./src/components/Main";
 
 export default function App() {
   return (
-    <NativeRouter>
-      <Main />
-    </NativeRouter>
+    <>
+      <StatusBar style="light" />
+      <NativeRouter>
+        <Main />
+      </NativeRouter>
+    </>
   );
 }
