f63407909c77e98c49a4974ff96a3b6067000dec
crcascr <cristianj_20@hotmail.com>
2024-01-17 18:04:37 -0500

    Queries a la REST API

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,12 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { FlatList, Text } from "react-native";
-import repositories from "../data/repositories";
 import RepositoryItem from "./RepositoryItem";
 
 function RepositoryList() {
+  const [repositories, setRepositories] = useState(null);
+
+  async function fetchRepositories() {
+    const response = await globalThis.fetch(
+      "http://localhost:5000/api/repositories"
+    );
+    const json = await response.json();
+    setRepositories(json);
+  }
+
+  useEffect(() => {
+    fetchRepositories();
+  }, []);
+
+  const repositoriesNodes = repositories
+    ? repositories.edges.map((edge) => edge.node)
+    : [];
+
   return (
     <FlatList
-      data={repositories}
+      data={repositoriesNodes}
       ItemSeparatorComponent={() => <Text></Text>}
       renderItem={({ item: repo }) => <RepositoryItem {...repo} />}
     ></FlatList>
